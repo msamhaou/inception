4881ba8179a13a23ea6d98963dd5c1ff575604f2
Mohamed-Taha Samhaoui <83616649+tahaekko@users.noreply.github.com>
2024-07-17 22:22:49 +0100

    WP-USER

--- src/wordpress/script.sh
+++ src/wordpress/script.sh
@@ -6,7 +6,7 @@ chmod +x wp-cli.phar
 ./wp-cli.phar core download --path=/var/www/wordpress --allow-root
 ./wp-cli.phar config create --path=/var/www/wordpress --dbname=$DB --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=mariadb:3306 --allow-root
 ./wp-cli.phar core install --url=$DOMAIN_NAME --title=$TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$EMAIL --path=/var/www/wordpress --allow-root
-./wp-cli.phar user create $WP_USER $WP_EMAIL --user_pass=$WP_EMAIL --path=/var/www/wordpress --allow-root
+./wp-cli.phar user create $WP_USER $WP_EMAIL --user_pass=$WP_PASS --path=/var/www/wordpress --allow-root
 mkdir -p /run/php/
 php-fpm7.4 -F --nodaemonize
 
